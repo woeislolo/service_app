be1925b20d13def1299f9c32eb78ae1c061d3686
woeislolo <falloutgirl7@mail.ru>
2025-05-28 15:48:13 +0500

    feat: add models to admin panel for app services

--- service/services/admin.py
+++ service/services/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    list_display_links = ('id', 'name', 'price')
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'discount_percent')
+    list_display_links = ('id', 'type', 'discount_percent')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'service', 'plan')
+    list_display_links = ('id', 'client', 'service', 'plan')
