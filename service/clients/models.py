0c42ee7be125abed01a99b9edbedf0296fd677ad
woeislolo <falloutgirl7@mail.ru>
2025-05-28 15:46:15 +0500

    feat: create models and migrations for app clients

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        verbose_name_plural = 'Companies'
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.user}, {self.company}'
+
