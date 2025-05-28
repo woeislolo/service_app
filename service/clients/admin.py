a7944a6b9b584b6d08f7c4be765d2b0adaa54136
woeislolo <falloutgirl7@mail.ru>
2025-05-28 15:47:35 +0500

    feat: add models to admin panel for app clients

--- service/clients/admin.py
+++ service/clients/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'company', 'address')
+    list_display_links = ('id', 'user', 'company', 'address')
