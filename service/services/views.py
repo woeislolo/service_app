1fa7ed6df225e64beae64747da08c7a847d0759f
woeislolo <falloutgirl7@mail.ru>
2025-05-29 11:52:32 +0500

    feat: add viewset for subscription

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class SubscriptionViewSet(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.select_related('client__user', 'plan').only(
+        'client__user__username',
+        'client__user__email',
+        'plan__type',
+    )
+    serializer_class = SubscriptionSerializer
